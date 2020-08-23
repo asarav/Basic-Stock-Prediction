0769397933a9e9d87207b7cbab40905c94ad7e33
Avinash Saravanan <asarav@umich.edu>
2020-08-23 20:51:10 +0200

    Add new metric methods

--- utils/timeSeriesMetricsCalculation.py
+++ utils/timeSeriesMetricsCalculation.py
@@ -12,6 +12,22 @@ class TimeSeriesMetricsCalculation:
         last5 = self.data.tail(5)["Close"].mean()
         return last5, last15, last100, last365
 
+    def movingAveragesHigh(self):
+        #Get last 365 days
+        last365 = self.data.tail(365)["High"].mean()
+        last100 = self.data.tail(100)["High"].mean()
+        last15 = self.data.tail(15)["High"].mean()
+        last5 = self.data.tail(5)["High"].mean()
+        return last5, last15, last100, last365
+
+    def movingAveragesLow(self):
+        #Get last 365 days
+        last365 = self.data.tail(365)["Low"].mean()
+        last100 = self.data.tail(100)["Low"].mean()
+        last15 = self.data.tail(15)["Low"].mean()
+        last5 = self.data.tail(5)["Low"].mean()
+        return last5, last15, last100, last365
+
     #Compute autocorrelation with different lag values
     def autocorrelation(self, lag=1):
         return self.data["Close"].autocorr(lag)
@@ -22,3 +38,9 @@ class TimeSeriesMetricsCalculation:
         last15 = self.data.tail(15)["Volume"].mean()
         last5 = self.data.tail(5)["Volume"].mean()
         return last5, last15, last100, last365
+
+    def totalDividends(self):
+        return self.data['Dividends'].sum()
+
+    def totalSplits(self):
+        return self.data['Stock Splits'].sum()
